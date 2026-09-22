bf368d755242973185256ed2e2a2395cb0751d8a
Ideas Group <info@ideasgroup.pk>
2026-09-22 14:50:54 +0500

    Initial commit: Ideas Group Builders & Developers website

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Disable cache in development so edits show immediately
+app.use((req, res, next) => {
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+    next();
+});
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public'), { etag: false, maxAge: 0 }));
+
+// Route for home
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Route for about us page
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/about.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`IdeasGroup server is running on http://localhost:${PORT}`);
+});
